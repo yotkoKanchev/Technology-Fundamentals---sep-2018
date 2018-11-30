ca611912d815fd419a790e4bdea8cf07b0f28fc0
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 11:30:03 +0200

    Add files via upload

---
+++ 04. Methods/Methods-Lab/P07_MathPower/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace P07_MathPower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = Math.Abs(double.Parse(Console.ReadLine()));
+            double power = Math.Abs(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine(CalculatePowerOfNumber(number,power));
+        }
+
+        private static double CalculatePowerOfNumber(double number, double power)
+        {
+            double result = Math.Pow(number, power);
+
+            return result;
+        }
+    }
+}
