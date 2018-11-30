30b6fa2a606ce065dbf307ceb8623bcc74a357ff
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 09:57:56 +0200

    Add files via upload

---
+++ 02. Data-Types-and-Variables/Data-Types-and-Variables-Exercises/01 SumDigits/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01SumDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int number = int.Parse(input);
+            int sum = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                sum += number % 10;
+                number /= 10;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
