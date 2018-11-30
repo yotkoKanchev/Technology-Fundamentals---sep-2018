a21f382d83a7c4ee407e371bd1e6426f9fe6513f
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 09:55:51 +0200

    Add files via upload

---
+++ 02. Data-Types-and-Variables/Data-Types-and-Variables-Lab/08 ConcatNames/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08ConcatNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = Console.ReadLine();
+            string secondName = Console.ReadLine();
+            string delimiter = Console.ReadLine();
+
+            Console.WriteLine(firstName+delimiter+secondName);
+        }
+    }
+}
