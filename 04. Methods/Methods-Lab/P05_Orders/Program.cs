7000a9ed583d83be83c1f37fe4e3eb7a83b9c14f
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 11:29:13 +0200

    Add files via upload

---
+++ 04. Methods/Methods-Lab/P05_Orders/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace P05_Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+            CalculatePrice(product, quantity);
+        }
+        private static void CalculatePrice(string product , int quantity)
+        {
+            double price = 0;
+
+            switch (product)
+            {
+                case "coffee":
+                    price = 1.50;
+                    break;
+                case "water":
+                    price = 1.00;
+                    break;
+                case "coke":
+                    price = 1.40;
+                    break;
+                case "snacks":
+                    price = 2.00;
+                    break;
+            }
+
+            Console.WriteLine($"{price * quantity}");
+        }
+    }
+}
