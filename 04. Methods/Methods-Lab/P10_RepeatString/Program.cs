934f7fff976d548de4260b566300be41583b1983
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 11:31:38 +0200

    Add files via upload

---
+++ 04. Methods/Methods-Lab/P10_RepeatString/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace P09_RepeatString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string data = Console.ReadLine();
+            int repeats = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(RepeatStringNTimes(data, repeats));
+        }
+
+        private static string RepeatStringNTimes(string data, int numberOfRepeats)
+        {
+            StringBuilder resultString = new StringBuilder();
+
+            for (int i = 0; i < numberOfRepeats; i++)
+            {
+                resultString.Append(data);
+            }
+
+            return resultString.ToString();
+        }
+    }
+}
