fb0c7e4a7968fece4343e2a29361261bc6975053
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 10:58:24 +0200

    Add files via upload

---
+++ 03. Arrays/Arrays-Lab/01 DayOfWeek/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace P01._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] days = new string[7]
+                {
+                    "Monday",
+                    "Tuesday",
+                    "Wednesday",
+                    "Thursday",
+                    "Friday",
+                    "Saturday",
+                    "Sunday",
+                };
+
+            int number = int.Parse(Console.ReadLine());
+            if (number<1 || number > 7)
+            {
+                Console.WriteLine("Invalid day!");
+            }
+            else
+            {
+                Console.WriteLine(days[number-1]);
+            }
+        }
+    }
+}
