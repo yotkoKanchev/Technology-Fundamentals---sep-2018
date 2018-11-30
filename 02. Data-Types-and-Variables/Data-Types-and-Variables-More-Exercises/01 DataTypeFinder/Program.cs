9c01385cccd04d890495bf52e1ecb7dc6f194962
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 10:04:24 +0200

    Add files via upload

---
+++ 02. Data-Types-and-Variables/Data-Types-and-Variables-More-Exercises/01 DataTypeFinder/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01DataTypeFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "END")
+                {
+                    break;
+                }
+
+                bool isBoolean = bool.TryParse(input, out bool boolean);
+                bool isInteger = int.TryParse(input, out int integer);
+                bool isCharacter = char.TryParse(input, out char chareacter);
+                bool isFloatingPoint = double.TryParse(input, out double floatingPoint);
+
+                if (isBoolean)
+                {
+                    Console.WriteLine($"{input} is boolean type");
+                }
+                else if (isInteger)
+                {
+                    Console.WriteLine($"{input} is integer type");
+                }
+                else if (isCharacter)
+                {
+                    Console.WriteLine($"{input} is character type");
+                }
+                else if (isFloatingPoint)
+                {
+                    Console.WriteLine($"{input} is floating point type");
+                }
+                else
+                {
+                    Console.WriteLine($"{input} is string type");
+                }
+            }
+        }
+    }
+}
