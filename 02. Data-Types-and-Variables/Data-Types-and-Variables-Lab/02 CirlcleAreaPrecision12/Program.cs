0cf6dd98e0b43b7917bc840ecf045517c889a4f4
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 09:51:14 +0200

    Add files via upload

---
+++ 02. Data-Types-and-Variables/Data-Types-and-Variables-Lab/02 CirlcleAreaPrecision12/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02CirlcleAreaPrecision12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = double.Parse(Console.ReadLine());
+
+            double area = Math.PI * r * r;
+
+            Console.WriteLine($"{area:f12}");
+
+
+        }
+    }
+}
