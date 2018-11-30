6348d8226ee48cb35a2255fbdd936058cfef9ecd
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 11:38:47 +0200

    Add files via upload

---
+++ 04. Methods/Methods-Exercise/P07_NxNMatrix/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace P07_NxNMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            PrintNxNMatrix(n);
+        }
+
+        public static void PrintNxNMatrix(int number)
+        {
+            for (int i = 0; i < number; i++)
+            {
+                for (int j = 0; j < number; j++)
+                {
+                    Console.Write(number+" ");
+                }
+                Console.WriteLine();
+            }            
+        }
+    }
+}
