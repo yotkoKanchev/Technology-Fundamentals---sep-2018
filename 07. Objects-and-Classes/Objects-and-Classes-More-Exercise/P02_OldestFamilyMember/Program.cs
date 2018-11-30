cdf4b95f7a85c5333de8f249e3c1b90b3ff53be1
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 12:18:36 +0200

    Add files via upload

---
+++ 07. Objects-and-Classes/Objects-and-Classes-More-Exercise/P02_OldestFamilyMember/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02_OldestFamilyMember
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Family family = new Family();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] tokens = Console.ReadLine().Split();
+
+                family.AdMember(tokens);
+            }
+
+            Person oldestPerson = family.GetOldestMember();
+
+            Console.WriteLine($"{oldestPerson.Name} {oldestPerson.Age}");
+        }
+        class Person
+        {
+            public Person(string[] inputArguments)
+            {
+                this.Name = inputArguments[0];
+                this.Age = int.Parse(inputArguments[1]);
+            }
+            public string Name { get; set; }
+            public int Age { get; set; }
+        }        
+
+        class Family
+        {
+            List<Person> FamilyMemebers { get; set; } = new List<Person>();
+
+            public void AdMember(string[] personData)
+            {
+                Person newMember = new Person(personData);
+                FamilyMemebers.Add(newMember);
+            }
+
+            public Person GetOldestMember()
+            {
+                return FamilyMemebers.OrderByDescending(x => x.Age).First();
+            }
+        }
+    }
+}
