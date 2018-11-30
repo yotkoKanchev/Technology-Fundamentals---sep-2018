12b8aacb7b8093da7acab1a087c11fa2ac45a0db
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 09:50:19 +0200

    Add files via upload

---
+++ 02. Data-Types-and-Variables/Data-Types-and-Variables-Lab/01 IntegerOperations/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01IntegerOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+            int fourthNum = int.Parse(Console.ReadLine());
+
+            int result = (firstNum + secondNum) / thirdNum * fourthNum;
+
+            Console.WriteLine(result);
+        }
+    }
+}
