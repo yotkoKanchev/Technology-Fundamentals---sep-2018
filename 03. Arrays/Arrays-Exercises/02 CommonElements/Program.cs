85019e66602124b95a68accec9abf46b9af01eee
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 11:06:01 +0200

    Add files via upload

---
+++ 03. Arrays/Arrays-Exercises/02 CommonElements/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstArray = Console.ReadLine().Split();
+            string[] secondArray = Console.ReadLine().Split();
+
+            List<string> resultList = new List<string>();
+
+            for (int i = 0; i < secondArray.Length; i++)
+            {
+                for (int j = 0; j < firstArray.Length; j++)
+                {
+                    if (secondArray[i] == firstArray[j])
+                    {
+                        resultList.Add(firstArray[j]);
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(" ", resultList));
+        }
+    }
+}
