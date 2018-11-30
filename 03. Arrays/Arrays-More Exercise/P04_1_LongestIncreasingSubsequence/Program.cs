ca979b1d8d715dde081dd7935ebf00fffacca80a
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 11:16:55 +0200

    Add files via upload

---
+++ 03. Arrays/Arrays-More Exercise/P04_1_LongestIncreasingSubsequence/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+
+namespace P04._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] temArray = new int[myArray.Length];
+            int[] resultArray = new int[myArray.Length];
+
+            int maxLength = 0;
+            int lastIndex = -1;
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                temArray[i] = 1;
+                resultArray[i] = -1;
+
+                for (int j = 0; j < i; j++)
+                {
+                    if (myArray[j] < myArray[i] && temArray[j] + 1 > temArray[i])
+                    {
+                        temArray[i] = temArray[j] + 1;
+                        resultArray[i] = j;
+                    }
+                }                    
+                if (temArray[i] > maxLength)
+                {
+                    maxLength = temArray[i];
+                    lastIndex = i;
+                }
+            }
+            int counter = 0;
+            int[] finalArray = new int[maxLength];
+            while (lastIndex != -1)
+            {
+                finalArray[counter] = myArray[lastIndex];
+                counter++;
+                lastIndex = resultArray[lastIndex];
+            }
+
+            Array.Sort(finalArray);
+
+            Console.WriteLine(string.Join(" ",finalArray));
+        }
+
+    }
+}
