6addfa5d9db010085687602e3736c279381aa088
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 11:52:46 +0200

    Add files via upload

---
+++ 05. Lists/Lists-Lab/P02_SumAdjacentEqualNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02_SumAdjacentEqualNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> numbers = Console.ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToList();
+
+            for (int i = 0; i < numbers.Count-1; i++)
+            {
+                if (numbers[i] == numbers[i+1])
+                {
+                    numbers[i] = 2 * numbers[i];
+                    numbers.RemoveAt(i + 1);
+                    i=-1;
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
