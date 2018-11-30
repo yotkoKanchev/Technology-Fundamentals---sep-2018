ea323a8fb731715e9c7a24cbb876220c2aaba6d9
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 11:27:21 +0200

    Add files via upload

---
+++ 04. Methods/Methods-Lab/P01_SignOfIntegerNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace P01_SignOfIntegerNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintSign(int.Parse(Console.ReadLine()));
+        }
+
+        static void PrintSign(int number)
+        {
+            if (number<0)
+            {
+                Console.WriteLine($"The number {number} is negative.");
+            }
+
+            else if (number>0)
+            {
+                Console.WriteLine($"The number {number} is positive.");
+            }
+
+            else
+            {
+                Console.WriteLine("The number 0 is zero.");
+            }
+        }
+    }
+}
