f689c59c6a406a5473969d2053042bb1724ab964
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 11:07:13 +0200

    Add files via upload

---
+++ 03. Arrays/Arrays-Exercises/04 ArrayRotation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> numbers = Console.ReadLine().Split().ToList();
+
+            int rotations = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < rotations % numbers.Count; i++)
+            {
+                numbers.Add(numbers[0]);
+                numbers.RemoveAt(0);
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
