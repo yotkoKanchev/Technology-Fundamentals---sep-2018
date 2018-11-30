002dbdd7e5d83596c2e66e5cb592cd3f4f54392f
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 11:45:14 +0200

    Add files via upload

---
+++ 04. Methods/Methods-More-Exercise/P04_TribonacciSequence/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P04_TribonacciSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            var listNumers = new List<int>();
+            listNumers.Add(1);
+
+            for (int i = 0; i < num-1; i++)
+            {
+                if (i < 3)
+                {
+                    listNumers.Add(listNumers.Sum());
+                }
+                else
+                {
+                    listNumers.Add(listNumers[i] + listNumers[i - 1] + listNumers[i - 2]);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", listNumers));
+        }
+    }
+}
