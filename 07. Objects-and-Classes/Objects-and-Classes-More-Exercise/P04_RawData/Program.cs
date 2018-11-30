2e0b56254c2f4b2f205c6a332247065ed10d3546
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 12:19:27 +0200

    Add files via upload

---
+++ 07. Objects-and-Classes/Objects-and-Classes-More-Exercise/P04_RawData/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P04_RawData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Car> cars = new List<Car>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] tokens = Console.ReadLine().Split();
+
+                Car car = new Car(tokens);
+
+                cars.Add(car);
+            }
+
+            string cargoCommand = Console.ReadLine();
+
+            if (cargoCommand == "fragile")
+            {
+                foreach (var car in cars.Where(x=>x.CarCargo.CargoType == "fragile" && x.CarCargo.CargoWeight < 1000))
+                {
+                    Console.WriteLine(car.Model);
+                }
+            }
+            else if (cargoCommand == "flamable")
+            {
+                foreach (var car in cars.Where(x=>x.CarCargo.CargoType == "flamable" && x.CarEngine.EnginePower > 250))
+                {
+                    Console.WriteLine(car.Model);
+                }
+            }
+        }
+
+        class Car
+        {
+
+            public Car(string[] tokens)
+            {
+                this.Model = tokens[0];
+                this.CarEngine = new Engine(tokens);
+                this.CarCargo = new Cargo(tokens);
+            }
+            public string Model { get; set; }
+            public Engine CarEngine { get; set; }
+            public Cargo CarCargo { get; set; }
+        }
+
+        class Engine
+        {
+            public Engine(string[] tokens)
+            {
+                this.EngineSpeed = int.Parse(tokens[1]);
+                this.EnginePower = int.Parse(tokens[2]);
+            }
+            public int EngineSpeed { get; set; }
+            public int EnginePower { get; set; }
+        }
+
+        class Cargo
+        {
+            public Cargo(string[] tokens)
+            {
+                this.CargoWeight = int.Parse(tokens[3]);
+                this.CargoType = tokens[4];
+            }
+            public int CargoWeight { get; set; }
+            public string CargoType { get; set; }
+        }
+    }
+}
