1a921da3cd609d838672eefc97735f430d73bb2f
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 09:29:14 +0200

    Add files via upload

---
+++ 01. Intro and Basic Syntax/Intro-and-Basic-Syntax-Exercise/01 Ages/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01Ages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+
+            string result = "";
+            if (age >=0 && age <=2)
+            {
+                result = "baby";
+            }
+            else if (age >= 3 && age <= 13)
+            {
+                result = "child";
+            }
+            else if (age >= 14 && age <= 19)
+            {
+                result = "teenager";
+            }
+            else if (age >= 20 && age <= 65)
+            {
+                result = "adult";
+            }
+            else if (age >= 66)
+            {
+                result = "elder";
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
