3c728dacb24b21bf6e97afd309db91513f74e8f4
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 11:28:48 +0200

    Add files via upload

---
+++ 04. Methods/Methods-Lab/P04_PrintingTriangle/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace P04_PrintingTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= number; i++)
+            {
+                PrintingSignleLine(1, i);
+            }
+
+            for (int i = number-1; i >= 1; i--)
+            {
+                PrintingSignleLine(1, i);
+            }
+        }
+
+        private static void PrintingSignleLine(int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
