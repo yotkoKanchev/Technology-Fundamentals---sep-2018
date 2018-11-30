7dae7f86dabc778185027bcb88eb0fb6b67774fb
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 12:00:01 +0200

    Add files via upload

---
+++ 05. Lists/Lists-Exercises/P03_HouseParty/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P03_HouseParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfCommands = int.Parse(Console.ReadLine());
+            List<string> namesList = new List<string>();
+            for (int i = 0; i < numberOfCommands; i++)
+            {
+                List<string> tokens = new List <string>(Console.ReadLine().Split());
+
+                if (tokens.Count == 4)
+                {
+                    if (namesList.Contains(tokens[0]) == true)
+                    {
+                        namesList.Remove(tokens[0]);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{tokens[0]} is not in the list!");
+                    }
+                }
+                else if (tokens.Count == 3)
+                {
+                    if (namesList.Contains(tokens[0]) == true)
+                    {
+                        Console.WriteLine($"{tokens[0]} is already in the list!");
+                    }
+                    else
+                    {
+                        namesList.Add(tokens[0]);
+                    }
+                }                
+            }
+            foreach (var name in namesList)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
