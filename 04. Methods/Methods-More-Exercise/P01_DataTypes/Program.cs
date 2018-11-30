0e6ebab0ba0d6194e273127cce6eb43e2c7eabeb
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 11:44:01 +0200

    Add files via upload

---
+++ 04. Methods/Methods-More-Exercise/P01_DataTypes/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace P01_DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dataType = Console.ReadLine();
+            string data = Console.ReadLine();
+
+            switch (dataType)
+            {
+                case "int":
+                    int number = int.Parse(data);
+                    Console.WriteLine(FormatInput(number));
+                    break;
+                case "real":
+                    double realNumber = double.Parse(data);
+                    Console.WriteLine($"{FormatInput(realNumber):f2}");
+                    break;
+                case "string":
+                    Console.WriteLine(FromatInput(data));
+                    break;
+            }
+        }
+        public static int FormatInput(int number)
+        {
+            return number * 2;
+        }
+
+        public static double FormatInput(double number)
+        {
+            return number * 1.5;
+        }
+        public static string FromatInput(string text)
+        {
+            string resultStirng = "$" + text + "$";
+            return resultStirng;
+        }
+    }
+}
