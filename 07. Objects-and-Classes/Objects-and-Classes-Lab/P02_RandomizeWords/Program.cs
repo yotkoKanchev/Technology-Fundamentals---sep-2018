d97b9913c07e8f5cbfdf404896972e62a41c734f
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 12:21:55 +0200

    Add files via upload

---
+++ 07. Objects-and-Classes/Objects-and-Classes-Lab/P02_RandomizeWords/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace P02._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+
+            Random newRandom = new Random();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                int randomIndex = newRandom.Next(0, words.Length);
+
+                string tempWord = words[i];
+
+                words[i] = words[randomIndex];
+                words[randomIndex] = tempWord;
+            }
+
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
