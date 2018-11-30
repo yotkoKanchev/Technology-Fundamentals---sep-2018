4620a05b16aa481fe37d216f05df3b1a6d810096
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 12:03:51 +0200

    Add files via upload

---
+++ 05. Lists/Lists-Exercises/P07_AppendArrays/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P07_AppendArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] numbers = Console.ReadLine().Split("|").ToArray();
+            Array.Reverse(numbers);
+
+            List<int> resultNumbers = new List<int>();
+
+            foreach (var element in numbers)
+            {
+                for (int i = 0; i < element.Split(new char[] { ' ' },StringSplitOptions.RemoveEmptyEntries).Length; i++)
+                {
+                    resultNumbers.Add(int.Parse(element.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)[i]));
+                }
+            }
+            Console.WriteLine(string.Join(" ", resultNumbers));
+                
+        }
+    }
+}
