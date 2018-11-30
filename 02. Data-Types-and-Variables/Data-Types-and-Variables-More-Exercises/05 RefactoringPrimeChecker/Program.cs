e8c41ce07f5e23f124bd0ca92c6393425cba2fd3
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 10:07:13 +0200

    Add files via upload

---
+++ 02. Data-Types-and-Variables/Data-Types-and-Variables-More-Exercises/05 RefactoringPrimeChecker/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05RefactoringPrimeChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 2; i <= n; i++)
+            {
+                bool isTrue = true;
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isTrue = false;
+                        break;
+                    }
+                }
+
+                Console.WriteLine($"{i} -> {isTrue.ToString().ToLower()}");
+            }
+
+        }
+    }
+}
