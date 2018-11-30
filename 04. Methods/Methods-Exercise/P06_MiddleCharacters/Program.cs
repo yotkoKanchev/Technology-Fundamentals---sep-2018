10d35d172572eab81767454312d3c2369611afc9
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 11:38:13 +0200

    Add files via upload

---
+++ 04. Methods/Methods-Exercise/P06_MiddleCharacters/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace P06_MiddleCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputData = Console.ReadLine();
+            Console.WriteLine(PrintMiddleCharacter(inputData));
+        }
+
+        public static string PrintMiddleCharacter(string text)
+        {
+            string result = string.Empty;
+
+            if (text.Length % 2 != 0)
+            {
+                result = text[text.Length / 2].ToString();
+            }
+            else
+            {
+                result = text[text.Length / 2 - 1].ToString() + text[text.Length / 2].ToString();
+            }
+
+            return result;
+        }
+    }
+}
