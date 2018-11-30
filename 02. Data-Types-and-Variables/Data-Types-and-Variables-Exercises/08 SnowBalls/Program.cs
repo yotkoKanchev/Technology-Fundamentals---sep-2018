a4bc5764049fed3a7f72ee885cedf0a8987f1f8b
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 10:02:40 +0200

    Add files via upload

---
+++ 02. Data-Types-and-Variables/Data-Types-and-Variables-Exercises/08 SnowBalls/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Numerics;
+
+namespace _08SnowBalls
+{
+    class Program
+    {
+        static void Main()
+        {
+            int numberOfSnowballs = int.Parse(Console.ReadLine());
+
+            int snowballSnow = 0;
+            int snowballTime = 0;
+            double snowballQuality = 0;
+
+            BigInteger snowballValue = 0;
+            for (int i = 1; i <= numberOfSnowballs; i++)
+            {
+                int snow = int.Parse(Console.ReadLine());
+                int time = int.Parse(Console.ReadLine());
+                int quality = int.Parse(Console.ReadLine());
+
+                BigInteger newSnowballValue = BigInteger.Pow((snow / time), quality);
+                if (newSnowballValue >= snowballValue)
+                {
+                    snowballValue = newSnowballValue;
+                    snowballSnow = snow;
+                    snowballTime = time;
+                    snowballQuality = quality;
+                }
+            }
+            Console.WriteLine($"{snowballSnow} : {snowballTime} = {snowballValue} ({snowballQuality})");
+        }
+    }
+}
