9ba091c1f5ed822f923bcec09576b5a2908498a8
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 11:00:57 +0200

    Add files via upload

---
+++ 03. Arrays/Arrays-Lab/04 ReverseArrayOfStrings/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace P04._11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] myArray = Console.ReadLine().Split();
+
+            Array.Reverse(myArray);
+
+            Console.WriteLine(string.Join(" ",myArray));                
+        }
+    }
+}
