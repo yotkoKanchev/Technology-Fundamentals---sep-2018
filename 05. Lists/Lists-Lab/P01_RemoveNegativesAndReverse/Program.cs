5c21a4f7941542d0cc8af73d95b4c1904beffcd8
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 11:52:22 +0200

    Add files via upload

---
+++ 05. Lists/Lists-Lab/P01_RemoveNegativesAndReverse/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01_RemoveNegativesAndReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] < 0)
+                {
+                    numbers.RemoveAt(i);
+                    i--;
+                }
+            }
+            numbers.Reverse();
+
+            if (numbers.Count<1)
+            {
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                Console.WriteLine(string.Join(" ", numbers));
+            }
+        }
+    }
+}
