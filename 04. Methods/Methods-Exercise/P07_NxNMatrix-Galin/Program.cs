404978fbeac980ae90008a4fda85ff84525997c0
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 11:39:13 +0200

    Add files via upload

---
+++ 04. Methods/Methods-Exercise/P07_NxNMatrix-Galin/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace P07_Galin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sizeMatrix = int.Parse(Console.ReadLine());
+
+            PrintMatrix(sizeMatrix);
+            
+        }
+
+        static void PrintMatrix(int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write(size + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
