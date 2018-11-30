ed0b525ec1e3d9ef4bcbe4d4c95b370f7814705c
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 09:40:17 +0200

    Add files via upload

---
+++ 01. Intro and Basic Syntax/Intro-and-Basic-Syntax-More-Exercise/02 EnglishNameOfTheLastDigit/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2EnglishNameOfTheLastDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int lastDigit = n % 10;
+
+            if (lastDigit == 1)
+            {
+                Console.WriteLine("one");
+            }
+            else if (lastDigit == 2)
+            {
+                Console.WriteLine("two");
+            }
+            else if (lastDigit == 3)
+            {
+                Console.WriteLine("three");
+            }
+            else if (lastDigit == 4)
+            {
+                Console.WriteLine("four");
+            }
+            else if (lastDigit == 5)
+            {
+                Console.WriteLine("five");
+            }
+            else if (lastDigit == 6)
+            {
+                Console.WriteLine("six");
+            }
+            else if (lastDigit == 7)
+            {
+                Console.WriteLine("seven");
+            }
+            else if (lastDigit == 8)
+            {
+                Console.WriteLine("eight");
+            }
+            else if (lastDigit == 9)
+            {
+                Console.WriteLine("nine");
+            }
+        }
+    }
+}
