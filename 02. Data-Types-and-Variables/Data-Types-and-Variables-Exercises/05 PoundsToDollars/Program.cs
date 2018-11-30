80f12b97de68c14759ba42136dc67049ac7851d2
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 10:00:25 +0200

    Add files via upload

---
+++ 02. Data-Types-and-Variables/Data-Types-and-Variables-Exercises/05 PoundsToDollars/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05PoundsToDollars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal pounds = decimal.Parse(Console.ReadLine());
+
+            decimal inDollars = pounds * 1.31m;
+
+            Console.WriteLine($"{inDollars:f3}");
+
+
+
+        }
+    }
+}
