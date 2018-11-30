818f5816e7543939a5c6286c2901044987ef9cbc
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 11:35:08 +0200

    Add files via upload

---
+++ 04. Methods/Methods-Exercise/P02_VowelsCount/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace P02_VowelsCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString = Console.ReadLine().ToLower();
+
+            Console.WriteLine(GetNumberOfVowels(inputString));
+        }
+
+        private static int GetNumberOfVowels(string data)
+        {
+            int result = 0;
+
+            foreach (var letter in data)
+            {
+                if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u') 
+                {
+                    result++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
