53fc4ee4d619e0573c43608fb592fa23c4405d20
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 11:55:04 +0200

    Add files via upload

---
+++ 05. Lists/Lists-Lab/P07_MergingList/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P07_MergingList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstList = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> secondList = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> resultList = new List<int>();
+
+            if (firstList.Count >= secondList.Count)
+            {
+                for (int i = 0; i < secondList.Count; i++)
+                {
+                    resultList.Add(firstList[i]);
+                    resultList.Add(secondList[i]);
+                }
+
+                for (int i = secondList.Count; i < firstList.Count; i++)
+                {
+                    resultList.Add(firstList[i]);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < firstList.Count; i++)
+                {
+                    resultList.Add(firstList[i]);
+                    resultList.Add(secondList[i]);
+                }
+
+                for (int i = firstList.Count; i < secondList.Count; i++)
+                {
+                    resultList.Add(secondList[i]);
+                }
+            }
+            Console.WriteLine(string.Join(" ", resultList));
+        }
+    }
+}
