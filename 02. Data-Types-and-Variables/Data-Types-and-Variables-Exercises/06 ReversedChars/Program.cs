bcd69565325b15d6af4aeb017a242bb28f256754
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 10:00:57 +0200

    Add files via upload

---
+++ 02. Data-Types-and-Variables/Data-Types-and-Variables-Exercises/06 ReversedChars/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06ReversedChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string first = Console.ReadLine();
+            string second = Console.ReadLine();
+            string third = Console.ReadLine();
+
+            Console.WriteLine($"{third} {second} {first}");
+
+        }
+    }
+}
