1655fa3af9350687df0ae14503c61b2688fffed1
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 11:41:45 +0200

    Add files via upload

---
+++ 04. Methods/Methods-Exercise/P10_TopNumber/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace P10_TopNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 17; i < number; i++)
+            {
+                if (CheckIfDevisableByEight(i) == true && CheckIfOddDigitExist(i) == true)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        public static bool CheckIfDevisableByEight(int number)
+        {
+            int sum = 0;
+            bool isDevisable = false;
+            while (number>0)
+            {
+                sum += number % 10;
+                number /= 10;
+            }
+
+            if (sum % 8 == 0)
+            {
+                isDevisable = true;
+            }
+            return isDevisable;
+        }
+
+        public static bool CheckIfOddDigitExist(int number)
+        {
+            bool oddDigitExist = false;
+
+            while (number>0)
+            {
+                if ((number % 10) % 2 != 0)
+                {
+                    oddDigitExist = true;
+                }
+                number /= 10;
+            }
+
+            return oddDigitExist;
+        }
+    }
+}
