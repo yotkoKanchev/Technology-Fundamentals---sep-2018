073adad224854738a74aaf4dab4abb1cfeca5462
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 11:29:38 +0200

    Add files via upload

---
+++ 04. Methods/Methods-Lab/P06_CalculateRectangleArea/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace P06_CalculateRectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(CalculateTriangleArea(width,height));
+        }
+
+        private static double CalculateTriangleArea(double width, double height)
+        {
+            double triangleArea = width * height;
+            return triangleArea;
+        }
+    }
+}
