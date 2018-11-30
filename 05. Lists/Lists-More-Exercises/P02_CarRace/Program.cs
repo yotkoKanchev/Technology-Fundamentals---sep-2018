c7fba26b5f95c9bb32eb727a0d074a06a3788ccc
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 11:56:21 +0200

    Add files via upload

---
+++ 05. Lists/Lists-More-Exercises/P02_CarRace/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02_CarRace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> minutes = Console.ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToList();
+            double firstCarTime = 0;
+            double secondCarTime = 0;
+
+            for (int i = 0; i < minutes.Count / 2; i++)
+            {
+                if (minutes[i] == 0)
+                {
+                    firstCarTime *= 0.8;
+                }
+                firstCarTime += minutes[i];
+            }
+
+            for (int i = minutes.Count-1; i > minutes.Count / 2; i--)
+            {
+                if (minutes[i] == 0)
+                {
+                    secondCarTime *= 0.8;
+                }
+                secondCarTime += minutes[i];
+            }
+            if (firstCarTime < secondCarTime)
+            {
+                Console.WriteLine($"The winner is left with total time: {firstCarTime}");
+            }
+            else if (secondCarTime < firstCarTime)
+            {
+                Console.WriteLine($"The winner is right with total time: {secondCarTime}");
+            }
+        
+        }
+    }
+}
