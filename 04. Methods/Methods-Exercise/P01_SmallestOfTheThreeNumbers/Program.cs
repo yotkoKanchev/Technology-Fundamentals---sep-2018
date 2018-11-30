e8a1370515f12b1837a42e634ce3076cf62b79e7
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 11:33:26 +0200

    Add files via upload

---
+++ 04. Methods/Methods-Exercise/P01_SmallestOfTheThreeNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace P01_SmallestOfTheThreeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetSmallestNumber(firstNumber, secondNumber, thirdNumber));
+        }
+
+        private static int GetSmallestNumber(int firstNum, int secondNum, int thirdsNum)
+        {
+            int[] resultArray = { firstNum, secondNum, thirdsNum };
+            int result = resultArray.Min();
+
+            return result;
+        }
+    }
+}
