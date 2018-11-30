01e3731542e13edc036cd0209536b9decae8fac0
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 11:14:33 +0200

    Add files via upload

---
+++ 03. Arrays/Arrays-More Exercise/P02_PascalTriangle/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02_PascalTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var pitagor = new List<int>();
+
+            pitagor.Add(1);
+            pitagor.Add(1);
+            if (n == 1)
+            {
+                Console.WriteLine(1);
+            }
+            else
+            {
+                Console.WriteLine(1);
+                Console.WriteLine($"1 1");
+            }
+
+            for (int i = 0; i < n - 2; i++)
+            {
+                var resultList = new List<int>();
+
+                for (int k = 0; k < pitagor.Count; k++)
+                {
+                    resultList.Add(pitagor[k]);
+                }
+                pitagor.Insert(pitagor.Count - 1, 1);
+                for (int j = 1; j < pitagor.Count-1; j++)
+                {
+                    resultList[j] = pitagor[j] + pitagor[j - 1];
+                }
+                
+                for (int p = 0; p < resultList.Count; p++)
+                {
+                    pitagor[p] = resultList[p];
+                }
+                Console.Write(string.Join(" ", pitagor));
+                Console.WriteLine();
+            }
+        }
+    }
+}
