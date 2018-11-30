0b2921f5d8f2a6b579665fe69f42488280d3ea95
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 09:54:12 +0200

    Add files via upload

---
+++ 02. Data-Types-and-Variables/Data-Types-and-Variables-Lab/06 SpecialNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06SpecialNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                int sum = i % 10 + i / 10;
+                if (sum == 5 || sum == 7 || sum == 11 )
+                {
+                    Console.WriteLine($"{i} -> True");
+                }
+                else
+                {
+                    Console.WriteLine($"{i} -> False");
+
+                }
+            }
+        }
+    }
+}
