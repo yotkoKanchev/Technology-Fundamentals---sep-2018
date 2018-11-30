2fa9d1951da976bee9008d5126e410d85bf84095
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 11:34:22 +0200

    Add files via upload

---
+++ 04. Methods/Methods-Exercise/P01_SmallestOfTheThreeNumbers-Galin/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace P01_Galin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+
+            int smallerNumber = SmallerNumber(firstNumber, secondNumber);
+
+            int result = SmallerNumber(smallerNumber, thirdNumber);
+
+            Console.WriteLine(result);
+        }
+        static int SmallerNumber(int firstNum, int seconNum)
+        {
+            return firstNum <= seconNum ? firstNum : seconNum;
+        }
+    }
+}
