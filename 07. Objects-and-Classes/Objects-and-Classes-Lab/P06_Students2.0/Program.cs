e99a3ac0a6d2d1dfcbdef22262a1f0ac2e2b4354
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 12:23:46 +0200

    Add files via upload

---
+++ 07. Objects-and-Classes/Objects-and-Classes-Lab/P06_Students2.0/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P06._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var projects = new Dictionary<string, Dictionary<string, List<string>>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] tokens = Console.ReadLine().Split("-");
+                string creatorName = tokens[0];
+                string teamToCreate = tokens[1];
+
+                if (!projects.ContainsKey(teamToCreate))
+                {
+                    if (!projects.Values))
+                    {
+
+                    }
+                }
+            }
+        }
+    }
+}
