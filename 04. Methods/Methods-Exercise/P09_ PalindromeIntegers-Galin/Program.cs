d15af76c7d39969a13f3414dee9b3eaeb41aec1c
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 11:41:15 +0200

    Add files via upload

---
+++ 04. Methods/Methods-Exercise/P09_ PalindromeIntegers-Galin/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace P09_Galin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string word = Console.ReadLine();
+                if (word == "END")
+                {
+                    break;
+                }
+                
+                string reversedWord = string.Empty;
+
+                for (int i = word.Length-1; i >= 0; i--)
+                {
+                    reversedWord += word[i];
+                }
+
+                bool isPalindrom = Palindrom(word, reversedWord);
+
+                Console.WriteLine(isPalindrom == true? "true" : "false");
+            }       
+
+        }
+
+        private static bool Palindrom(string word, string reversedWord)
+        {
+            return word == reversedWord;
+        }
+    }
+}
