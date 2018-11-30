029894363a101118c099103ae951b8e5c9d3b3db
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 09:31:51 +0200

    Add files via upload

---
+++ 01. Intro and Basic Syntax/Intro-and-Basic-Syntax-Exercise/05 Login/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05Login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+
+            string password = "";
+            int endLoop = username.Length - 1;
+            for (int i = endLoop; i >= 0; i--)
+            {
+                password += username[i];
+            }
+
+            string input = Console.ReadLine();
+            int counter = 0;
+            while (input != password)
+            {
+                
+                counter++;
+                if (counter > 3)
+                {
+                    Console.WriteLine($"User {username} blocked!");
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect password. Try again.");
+                }
+                
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"User {username} logged in.");
+        }   
+    }
+}
