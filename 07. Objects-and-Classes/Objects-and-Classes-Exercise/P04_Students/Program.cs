029e6102c75a6071628f9e7c4230858c79e56a06
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 12:35:36 +0200

    Add files via upload

---
+++ 07. Objects-and-Classes/Objects-and-Classes-Exercise/P04_Students/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P04_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<Student> students = new List<Student>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] tokens = Console.ReadLine().Split();
+
+                Student student = new Student(tokens);
+
+                students.Add(student);
+            }
+            Console.WriteLine(string.Join(Environment.NewLine, students.OrderByDescending(x => x.Grade)));
+        }
+
+        class Student
+        {
+            public Student(string[] arguments)
+                {
+                this.FirtName = arguments[0];
+                this.LastName = arguments[1];
+                this.Grade = double.Parse(arguments[2]);
+            }
+            public string FirtName { get; set; }
+            public string LastName { get; set; }
+            public double Grade { get; set; }
+
+            public override string ToString()
+            {
+                return $"{this.FirtName} {this.LastName}: {this.Grade:f2}";
+            }
+        }
+    }
+}
