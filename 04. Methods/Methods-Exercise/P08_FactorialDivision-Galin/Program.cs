a7ba45c90f3e5bd2bc05c642a1156625f1c6e8a7
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 11:40:05 +0200

    Add files via upload

---
+++ 04. Methods/Methods-Exercise/P08_FactorialDivision-Galin/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace P08_Galin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            long firstFactorial = Factorial(firstNumber);
+            long secondFactorial = Factorial(secondNumber);
+
+            double result = Divide(firstFactorial, secondFactorial);
+
+            Console.WriteLine($"{result:f2}");
+        }
+
+        static double Divide(long num1,long num2)
+        {
+            return (double)num1 / num2;
+        }
+
+        private static long Factorial(long num)
+        {
+            long factorial = 1;
+
+            for (int i = 2; i <= num; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+    }
+}
