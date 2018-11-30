c2e14f4d766d6f06ba6cbdc3c7ace3dcbbd556b4
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 09:52:55 +0200

    Add files via upload

---
+++ 02. Data-Types-and-Variables/Data-Types-and-Variables-Lab/04 Elevator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfpeople = int.Parse(Console.ReadLine());
+
+            int capacity = int.Parse(Console.ReadLine());
+
+            int courses = numberOfpeople / capacity;           
+
+            if (numberOfpeople % capacity != 0)
+            {
+                courses += 1;
+            }
+            Console.WriteLine(courses);
+        }
+    }
+}
