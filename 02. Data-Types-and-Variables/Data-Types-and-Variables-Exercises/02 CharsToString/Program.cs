bc5553be74ec2b5a1343fe5c10a4f9ea63574462
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 09:58:40 +0200

    Add files via upload

---
+++ 02. Data-Types-and-Variables/Data-Types-and-Variables-Exercises/02 CharsToString/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02CharsToString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstInput = char.Parse(Console.ReadLine());
+            char secondInput = char.Parse(Console.ReadLine());
+            char thirdInput = char.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{firstInput}{secondInput}{thirdInput}");
+        }
+    }
+}
