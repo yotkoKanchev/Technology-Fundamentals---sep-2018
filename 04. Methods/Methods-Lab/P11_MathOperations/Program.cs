812af314b17577fc616b193edd3d089d37485f11
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 11:32:35 +0200

    Add files via upload

---
+++ 04. Methods/Methods-Lab/P11_MathOperations/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace P11_MathOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNumber = double.Parse(Console.ReadLine());
+            string @operator = Console.ReadLine();
+            double secondNumber = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(CalculateResult(firstNumber, @operator, secondNumber));
+        }
+
+        private static double CalculateResult(double firstNum, string @operator, double secondNum)
+        {
+            double result = 0;
+            switch (@operator)
+            {
+                case "/":
+                    result = firstNum / secondNum;
+                    break;
+                case "*":
+                    result = firstNum * secondNum;
+                    break;
+                case "+":
+                    result = firstNum + secondNum;
+                    break;
+                case "-":
+                    result = firstNum - secondNum;
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
