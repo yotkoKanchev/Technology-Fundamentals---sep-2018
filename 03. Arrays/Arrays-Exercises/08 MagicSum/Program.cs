09d22d4bb7c2af4be91466c3cd68b6908eaf4881
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 11:09:24 +0200

    Add files via upload

---
+++ 03. Arrays/Arrays-Exercises/08 MagicSum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08._11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            int sumNumber = int.Parse(Console.ReadLine());
+
+            for (int i = 0;  i < numbers.Count-1; i++)
+            {
+                for (int j = i+1; j < numbers.Count; j++)
+                {
+                    if (numbers[i] + numbers[j] == sumNumber)
+                    {
+                        Console.WriteLine($"{numbers[i]} {numbers[j]}");
+                    }
+                }
+            }
+        }
+    }
+}
