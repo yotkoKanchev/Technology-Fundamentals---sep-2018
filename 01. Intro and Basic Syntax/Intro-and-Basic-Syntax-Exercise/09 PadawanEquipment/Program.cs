43140aa1b46930124f576cc47262c4155bba2a16
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 09:34:45 +0200

    Add files via upload

---
+++ 01. Intro and Basic Syntax/Intro-and-Basic-Syntax-Exercise/09 PadawanEquipment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09PadawanEquipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double money = double.Parse(Console.ReadLine());
+            double studentsCount = double.Parse(Console.ReadLine());
+            double sabersPrice = double.Parse(Console.ReadLine());
+            double robesPrice = double.Parse(Console.ReadLine());
+            double beltsPrice = double.Parse(Console.ReadLine());
+
+            double totalMoney = sabersPrice * Math.Ceiling(studentsCount + 0.1 * studentsCount) + robesPrice * studentsCount + beltsPrice * Math.Ceiling(studentsCount - studentsCount / 6);
+
+            if (totalMoney <= money)
+            {
+                Console.WriteLine($"The money is enough - it would cost {totalMoney:f2}lv.");
+            }
+            else
+            {
+                Console.WriteLine($"Ivan Cho will need {(totalMoney-money):f2}lv more.");
+            }
+        }
+    }
+}
