6b975a0475bb948f6f137218eba13fa569dc9fea
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 10:06:21 +0200

    Add files via upload

---
+++ 02. Data-Types-and-Variables/Data-Types-and-Variables-More-Exercises/04 FloatingEquality/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02FloatingEquality
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNum = double.Parse(Console.ReadLine());
+            double secondNum = double.Parse(Console.ReadLine());
+
+            double result = Math.Abs(firstNum - secondNum);
+
+            if (result > 0.000001)
+            {
+                Console.WriteLine("False");
+            }
+            else
+            {
+                Console.WriteLine("True");
+            }
+        }
+    }
+}
