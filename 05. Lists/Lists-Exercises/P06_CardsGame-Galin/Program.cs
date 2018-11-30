3d9e4e9b6d281837ca07c0d3cfe30b3250b258e9
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 12:03:15 +0200

    Add files via upload

---
+++ 05. Lists/Lists-Exercises/P06_CardsGame-Galin/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P06_Galin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstPlayerCards = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> secondPlayerCards = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            while (firstPlayerCards.Count != 0 && secondPlayerCards.Count != 0)
+            {
+                int firstPlayerCard = firstPlayerCards[0];
+                int secondPlayerCard = secondPlayerCards[0];
+
+                if (firstPlayerCard > secondPlayerCard)
+                {
+                    RemoveCards(firstPlayerCards, secondPlayerCards);
+
+                    firstPlayerCards.Add(firstPlayerCard);
+                    firstPlayerCards.Add(secondPlayerCard);
+                }
+                else if (secondPlayerCard > firstPlayerCard)
+                {
+                    RemoveCards(firstPlayerCards, secondPlayerCards);
+
+                    secondPlayerCards.Add(secondPlayerCard);
+                    secondPlayerCards.Add(firstPlayerCard);
+                }
+                else
+                {
+                    RemoveCards(firstPlayerCards, secondPlayerCards);
+                }
+            }
+            if (firstPlayerCards.Count>0)
+            {
+                int sum = firstPlayerCards.Sum();
+                Console.WriteLine($"First player wins! Sum: {sum}");
+            }
+            else if (secondPlayerCards.Count>0)
+            {
+                int sum = secondPlayerCards.Sum();
+                Console.WriteLine($"Second player wins! Sum: {sum}");
+            }
+        }
+        static void RemoveCards(List<int> firstPlayerCards, List<int> secondPlayerCards)
+        {
+            firstPlayerCards.RemoveAt(0);
+            secondPlayerCards.RemoveAt(0);
+        }
+    }
+}
