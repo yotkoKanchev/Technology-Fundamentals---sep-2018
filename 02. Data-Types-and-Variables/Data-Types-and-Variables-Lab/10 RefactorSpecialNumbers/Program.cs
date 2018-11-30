fa674c8c9dcb68830b218b40b3e6f05d1af054bb
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 09:57:00 +0200

    Add files via upload

---
+++ 02. Data-Types-and-Variables/Data-Types-and-Variables-Lab/10 RefactorSpecialNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10RefactorSpecialNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int total = 0;
+            int currentNumber = 0;
+            bool isTrue = false;
+            for (int i = 1; i <= number; i++)
+            {
+                currentNumber = i;
+                while (i > 0)
+                {
+                    total += i % 10;
+                    i = i / 10;
+                }
+                isTrue = (total == 5) || (total == 7) || (total == 11);
+                Console.WriteLine($"{currentNumber} -> {isTrue}");
+                total = 0;
+                i = currentNumber;
+
+            }
+        }
+    }
+}
