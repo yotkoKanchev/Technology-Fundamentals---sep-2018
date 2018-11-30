c878c2453658daeabdc3571b9ef5e9a5774fad77
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 09:41:53 +0200

    Add files via upload

---
+++ 01. Intro and Basic Syntax/Intro-and-Basic-Syntax-More-Exercise/04 ReverseString/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            for (int i = input.Length-1; i >= 0; i--)
+            {
+                Console.Write(input[i]);
+            }
+        }
+    }
+}
