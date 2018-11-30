40928bb595db6725568245e42b165b193237a605
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 11:39:41 +0200

    Add files via upload

---
+++ 04. Methods/Methods-Exercise/P08_FactorialDivision/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Numerics;
+
+namespace P08_FactorialDivision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger firstNumber = BigInteger.Parse(Console.ReadLine());
+            BigInteger secondNumber =BigInteger.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{GetFactorielDivision(firstNumber,secondNumber):f2}");
+        }
+
+        public static decimal GetFactorielDivision(BigInteger num1, BigInteger num2)
+        {
+            return GetFactoriel(num1) / GetFactoriel(num2);
+        }
+
+        public static decimal GetFactoriel(BigInteger num)
+        {
+            decimal result = 1;
+
+            if (num == 0)
+            {
+               result = 1;
+            }
+            else
+            {
+                for (int i = 1; i <= num; i++)
+                {
+                    result *= i;
+                }
+            }            
+            return result;
+        }
+    }
+}
