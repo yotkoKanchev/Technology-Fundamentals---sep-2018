07c071b0dd84b6470b93a86edf7b31ebc23b803a
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 10:04:59 +0200

    Add files via upload

---
+++ 02. Data-Types-and-Variables/Data-Types-and-Variables-More-Exercises/02 FromLeftToTheRight/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Numerics;
+
+namespace _02FromLeftToTheRight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string arguments = Console.ReadLine();
+                BigInteger firstNum = BigInteger.Parse(arguments.Split()[0]);
+                BigInteger secondNum = BigInteger.Parse(arguments.Split()[1]);
+
+                int sum = 0;
+                if (firstNum > secondNum)
+                {
+                    foreach (var item in arguments.Split(' ')[0])
+                    {
+                        if (item =='-')
+                        {
+                            continue;
+                        }
+                        sum += int.Parse(item.ToString());
+                    }                
+                    Console.WriteLine(sum);
+                }
+                else
+                {
+                    foreach (var item in arguments.Split(' ')[1])
+                    {
+                        if (item == '-')
+                        {
+                            continue;
+                        }
+                        sum += int.Parse(item.ToString());
+                    }
+                    Console.WriteLine(sum);
+                }
+            }           
+            
+        }
+    }
+}
