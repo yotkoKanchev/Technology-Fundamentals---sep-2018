1e1355c52873511c2ef40e185d954c14cc02a4c7
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 11:37:37 +0200

    Add files via upload

---
+++ 04. Methods/Methods-Exercise/P05_AddAndSubstract/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace P05_AddAndSubstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(AddAndSubtractNumbers(firstNumber,secondNumber,thirdNumber));
+        }
+
+        public static int AddAndSubtractNumbers(int firstNum,int secondNum ,int thirdNum)
+        {
+            return firstNum + secondNum - thirdNum;
+        }
+    }
+}
