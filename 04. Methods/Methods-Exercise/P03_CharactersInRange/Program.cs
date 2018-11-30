4e175bc0d8956366f765e1f46893ff54f3e80f3a
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 11:35:40 +0200

    Add files via upload

---
+++ 04. Methods/Methods-Exercise/P03_CharactersInRange/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace P03_CharactersInRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstSimbol = char.Parse(Console.ReadLine());
+            char secondSimbol = char.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetSimbolsBetween(firstSimbol,secondSimbol));
+        }
+
+        private static string GetSimbolsBetween(char firstChar, char secondChar)
+        {
+
+            if (firstChar > secondChar)
+            {
+                char oldFirstChar = firstChar;
+                firstChar = secondChar;
+                secondChar = oldFirstChar;
+            }
+            string result = string.Empty;
+            for (int i = firstChar + 1; i < secondChar; i++)
+            {
+                result += (char)i + " ";
+            }
+            return result;
+        }
+    }
+}
