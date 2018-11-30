653eaada576afaec824c6f53d5f22d3d6404887c
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 11:06:37 +0200

    Add files via upload

---
+++ 03. Arrays/Arrays-Exercises/03 ZigZagArrays/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _03._11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfLines = int.Parse(Console.ReadLine());
+            string[] firstArray = new string[numberOfLines];
+            string[] secondArray = new string[numberOfLines];
+
+            int counter = 0;
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                string[] inputline = Console.ReadLine().Split();
+                if (counter % 2 == 0)
+                {
+                    firstArray[i] = inputline[0];
+                    secondArray[i] = inputline[1];
+                }
+                else
+                {
+                    firstArray[i] = inputline[1];
+                    secondArray[i] = inputline[0];
+                }
+                counter++;
+            }
+            Console.WriteLine(string.Join(" ",firstArray));
+            Console.WriteLine(string.Join(" ",secondArray));
+        }
+    }
+}
