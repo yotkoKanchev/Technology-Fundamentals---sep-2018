c8f4efdfdf30d171a54e8b3e92935bbf8d6eb92e
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 10:03:13 +0200

    Add files via upload

---
+++ 02. Data-Types-and-Variables/Data-Types-and-Variables-Exercises/09 PokeMon/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09PokeMon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pokePower = int.Parse(Console.ReadLine());
+            int distance = int.Parse(Console.ReadLine());
+            int exhaustionFactor = int.Parse(Console.ReadLine());
+
+            int discountedPokePower = pokePower;
+
+            int count = 0;
+
+            while (discountedPokePower >= distance)
+            {
+                discountedPokePower -= distance;
+                count += 1;
+
+                if (discountedPokePower == pokePower * 0.5 && exhaustionFactor > 0)
+                {
+                    discountedPokePower = discountedPokePower / exhaustionFactor;
+                }         
+
+            }
+            Console.WriteLine(discountedPokePower);
+            Console.WriteLine(count);
+        }
+    }
+}
