5e670e896fccddc4badd0f71b92fa80546c94c30
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 11:53:47 +0200

    Add files via upload

---
+++ 05. Lists/Lists-Lab/P04_ListOfProducts/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P04_ListOfProducts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var listOfStrings = new List<string>();
+           
+            for (int i = 0; i < n; i++)
+            {
+                string lineInput = Console.ReadLine();
+                listOfStrings.Add(lineInput);
+            }
+            listOfStrings.Sort();
+
+            for (int i = 0; i < listOfStrings.Count; i++)
+            {
+                Console.WriteLine($"{i+1}.{listOfStrings[i]}");
+            }
+        }
+    }
+}
