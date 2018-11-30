069ce7289542a34e06b9aa494c79ccc8c78cea26
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 11:28:24 +0200

    Add files via upload

---
+++ 04. Methods/Methods-Lab/P03_Calculations/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace P03_Calculations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine().ToLower();
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            switch (command)
+            {
+                case "add":
+                    AddNumbers(firstNumber, secondNumber);
+                    break;
+                case "multiply":
+                    MultiplyNumbers(firstNumber, secondNumber);
+                    break;
+                case "subtract":
+                    SubtractNumbers(firstNumber, secondNumber);
+                    break;
+                case "divide":
+                    DivideNumbers(firstNumber, secondNumber);
+                    break;
+            }
+        }
+
+        private static void AddNumbers(int firstNum,int seconNum)
+        {
+            Console.WriteLine(firstNum+seconNum);
+        }
+
+        private static void MultiplyNumbers(int firsNum, int secondNum)
+        {
+            Console.WriteLine(firsNum * secondNum);
+        }
+
+        private static void SubtractNumbers(int firstNum, int secondNum)
+        {
+            Console.WriteLine(firstNum-secondNum);
+        }
+
+        private static void DivideNumbers(int firstNum, int secondNum)
+        {
+            Console.WriteLine(firstNum / secondNum);
+        }
+    }
+}
