a6713aa27fc7cc86c036b5fffcbf86e820de6227
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 09:23:04 +0200

    Add files via upload

---
+++ 01. Intro and Basic Syntax/Intro-and-Basic-Syntax-Lab/11 MultiplicationTable2.0/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11MultiplicationTable2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int multiplayer = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+
+            if (n > 10)
+            {
+                Console.WriteLine($"{multiplayer} X {n} = {multiplayer * n}");
+            }
+            for (int i = n; i <= 10; i++)
+            {
+                Console.WriteLine($"{multiplayer} X {i} = {multiplayer * i}");
+            }
+        }
+    }
+}
