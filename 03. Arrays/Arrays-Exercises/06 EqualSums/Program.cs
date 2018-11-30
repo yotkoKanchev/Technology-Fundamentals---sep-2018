f1a7c497cd8d6a353faef19f1bda24681b41189d
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 11:08:15 +0200

    Add files via upload

---
+++ 03. Arrays/Arrays-Exercises/06 EqualSums/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            int index = -1;
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                int leftSum = numbers.Take(i).Sum();
+                int rightSum = numbers.Skip(i+1).Take(numbers.Count - i).Sum();
+                if (leftSum == rightSum)
+                {
+                    index = i;
+                }
+            }
+
+            if (index == -1)
+            {
+                Console.WriteLine("no");
+            }
+            else
+            {
+                Console.WriteLine(index);
+            }
+        }
+    }
+}
