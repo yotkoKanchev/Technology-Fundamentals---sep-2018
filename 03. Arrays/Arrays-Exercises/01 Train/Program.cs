fba43ff2e06b020cee67ac125864b5a26e9f8654
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 11:05:29 +0200

    Add files via upload

---
+++ 03. Arrays/Arrays-Exercises/01 Train/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfWagons = int.Parse(Console.ReadLine());
+            List<int> train = new List<int>();
+
+            for (int i = 0; i < numberOfWagons; i++)
+            {
+                train.Add(int.Parse(Console.ReadLine()));
+            }
+            Console.WriteLine(string.Join(" ",train));
+            Console.WriteLine(train.Sum());
+        }
+    }
+}
