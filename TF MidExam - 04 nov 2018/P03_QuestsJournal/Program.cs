6493f4215310b9bb7c28e9b416dcecd8d33843dc
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 13:36:42 +0200

    Add files via upload

---
+++ TF MidExam - 04 nov 2018/P03_QuestsJournal/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P03_QuestsJournal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> journal = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();                
+
+            string inputLine = Console.ReadLine();
+
+            while (inputLine != "Retire!")
+            {
+                string[] tokens = inputLine.Split(" - ", StringSplitOptions.RemoveEmptyEntries);
+                string command = tokens[0];
+
+                switch (command)
+                {
+                    case "Start":
+                        string quest = tokens[1];
+
+                        if (!journal.Contains(quest))
+                        {
+                            journal.Add(quest);
+                        }
+                        break;
+
+                    case "Complete":
+                        quest = tokens[1];
+                        if (journal.Contains(quest))
+                        {
+                            journal.Remove(quest);
+                        }
+                        break;
+
+                    case "Side Quest":
+                        string[] arguments = tokens[1].Split(":", StringSplitOptions.RemoveEmptyEntries);
+                        quest = arguments[0];
+                        string sideQuest = arguments[1];
+                        if (journal.Contains(quest) && journal.Contains(sideQuest) == false)
+                        {
+                            journal.Insert(journal.IndexOf(quest)+1, sideQuest);
+                        }
+
+                        break;
+
+                    case "Renew":
+                        quest = tokens[1];
+
+                        if (journal.Contains(quest))
+                        {
+                            journal.Remove(quest);
+                            journal.Add(quest);
+                        }
+                        break;                   
+                }
+
+                inputLine = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join(", ",journal));
+        }
+    }
+}
