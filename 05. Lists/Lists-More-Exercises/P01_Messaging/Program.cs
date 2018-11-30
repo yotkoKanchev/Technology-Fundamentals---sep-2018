cf7f898e56529caea1ea5493f17175f27857d858
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 11:55:54 +0200

    Add files via upload

---
+++ 05. Lists/Lists-More-Exercises/P01_Messaging/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01_Messaging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()     
+                .Select(int.Parse)           
+                .ToList();
+
+            List<char> text = Console.ReadLine().ToList();
+            
+            int digitsSum = 0;
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                while (numbers[i] > 0)
+                {
+                    digitsSum += numbers[i] % 10;
+                    numbers[i] /= 10;
+                }
+                if (digitsSum < text.Count)
+                {
+                    Console.Write(text[digitsSum]);
+                    text.RemoveAt(digitsSum);
+                }
+                else
+                {
+                    digitsSum = digitsSum - text.Count;
+                    Console.Write(text[digitsSum]);
+                    text.RemoveAt(digitsSum);
+                }
+                digitsSum = 0;
+            }
+            Console.WriteLine();
+        }
+    }
+}
