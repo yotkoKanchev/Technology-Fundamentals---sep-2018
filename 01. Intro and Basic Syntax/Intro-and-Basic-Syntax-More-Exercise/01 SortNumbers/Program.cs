befa8470b2a6ed71b3c11bfaf7d9aae454c4e36c
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 09:38:58 +0200

    Add files via upload

---
+++ 01. Intro and Basic Syntax/Intro-and-Basic-Syntax-More-Exercise/01 SortNumbers/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01Sort_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+
+            int minNum = 0;
+            int midNum = 0;
+            int maxNum = 0;
+            if (firstNum > secondNum && firstNum > thirdNum)
+            {
+                maxNum = firstNum;
+                if (secondNum > thirdNum)
+                {
+                    midNum = secondNum;
+                    minNum = thirdNum;
+                }
+                else
+                {
+                    midNum = thirdNum;
+                    minNum = secondNum;
+                }
+            }
+            else if (secondNum > firstNum && secondNum > thirdNum)
+            {
+                maxNum = secondNum;
+                if (firstNum > thirdNum)
+                {
+                    midNum = firstNum;
+                    minNum = thirdNum;
+                }
+                else
+                {
+                    midNum = thirdNum;
+                    minNum = firstNum;
+                }
+            }
+            else if (thirdNum > firstNum && thirdNum > secondNum)
+            {
+                maxNum = thirdNum;
+                if (firstNum > secondNum)
+                {
+                    midNum = firstNum;
+                    minNum = secondNum;
+                }
+                else
+                {
+                    midNum = secondNum;
+                    minNum = firstNum;
+                }
+            }
+            Console.WriteLine(maxNum);
+            Console.WriteLine(midNum);
+            Console.WriteLine(minNum);
+        }
+    }
+}
