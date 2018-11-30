83a95ff3c02889bb7704b98a1fb4f590588e9ab8
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 09:22:00 +0200

    Add files via upload

---
+++ 01. Intro and Basic Syntax/Intro-and-Basic-Syntax-Lab/09 SumOffOddNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09SumOffOddNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int num = 1;
+            int sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(num);
+                sum += num;
+                num += 2;              
+                
+            }
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
