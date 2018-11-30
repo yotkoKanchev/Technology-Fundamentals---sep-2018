b7805155a8d077f676af679479ca840879a1d9e3
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 11:30:58 +0200

    Add files via upload

---
+++ 04. Methods/Methods-Lab/P09_MultiplyEvensByOdds/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace P09_MultiplyEvensByOdds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Math.Abs(int.Parse(Console.ReadLine()));
+
+            int printResult = GetMultiplyOfEvenAndOdds(GetSumOfEvenDigits(number), GetSumOfOddDigits(number));
+            Console.WriteLine(printResult);
+        }
+
+        private static int GetSumOfEvenDigits(int number)
+        {
+            int evenSum = 0;
+
+            while (number>0)
+            {
+                if ((number % 10 )% 2 == 0)
+                {
+                    evenSum += number % 10;
+                }
+                    number /= 10;
+            }
+            return evenSum;
+        }
+
+        private static int GetSumOfOddDigits(int number)
+        {
+            int oddSum = 0;
+
+            while (number > 0)
+            {
+                if ((number % 10) %2 == 1)
+                {
+                    oddSum += number % 10;
+                }
+                number /= 10;
+            }
+            return oddSum;
+        }
+
+        private static int GetMultiplyOfEvenAndOdds(int sumOfEvenDigits, int sumOfOddDigits)
+        {
+            int printResult = sumOfEvenDigits * sumOfOddDigits;
+            return printResult;
+        }
+    }
+}
