dd19cc709ec2c7b6161448fcb83624b7a9c97b60
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 09:18:45 +0200

    Add files via upload

---
+++ 01. Intro and Basic Syntax/Intro-and-Basic-Syntax-Lab/04 BackIn30Minutes/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04BackIn30Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            int totalminutes = hours * 60 + minutes + 30;
+
+            int newHours = 0;
+            int newMinutes = 0;
+
+            if (totalminutes < 60)
+            {
+                Console.WriteLine($"0:{totalminutes:D2}");
+            }
+            else
+            {
+                newHours = totalminutes / 60;
+                newMinutes = totalminutes % 60;
+
+                if (newHours > 23)
+                {
+                    newHours = newHours - 24;
+                }
+                
+            }
+            Console.WriteLine($"{newHours}:{newMinutes:D2}");
+        }
+    }
+}
