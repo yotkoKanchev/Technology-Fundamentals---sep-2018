0f3c2c8350285d290ee6fe836ee00bdbc366fd07
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 11:04:12 +0200

    Add files via upload

---
+++ 03. Arrays/Arrays-Lab/08 CondenseArrayToNumber/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P08._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+                        
+            while (numbers.Count>1)
+            {
+                for (int i = 0; i < numbers.Count - 1; i++)
+                {
+                    numbers.Insert(i + 1, numbers[i] + numbers[i + 1]);
+                    numbers.RemoveAt(i);
+                }
+                numbers.RemoveAt(numbers.Count - 1);
+            }
+            Console.WriteLine(numbers[0]);
+        }
+    }
+}
