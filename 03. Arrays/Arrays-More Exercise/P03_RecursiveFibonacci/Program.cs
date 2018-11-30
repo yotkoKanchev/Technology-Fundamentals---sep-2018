8580f2ede311b82fd7a9fd6b0eab91954fc2b862
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 11:15:24 +0200

    Add files via upload

---
+++ 03. Arrays/Arrays-More Exercise/P03_RecursiveFibonacci/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace P03._11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fibonacciNumber = int.Parse(Console.ReadLine());
+            if (fibonacciNumber == 1)
+            {
+                Console.WriteLine(1);
+            }
+            else
+            {
+                int[] myArray = new int[fibonacciNumber];
+                myArray[0] = 1;
+                myArray[1] = 1;
+
+                for (int i = 2; i < fibonacciNumber; i++)
+                {
+                    myArray[i] = myArray[i - 1] + myArray[i - 2];
+                }
+
+                Console.WriteLine(myArray[fibonacciNumber - 1]);
+            }            
+        }
+    }
+}
