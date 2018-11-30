c04f69c7940201c30edb6ad4b0e93134a1f71201
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 10:05:46 +0200

    Add files via upload

---
+++ 02. Data-Types-and-Variables/Data-Types-and-Variables-More-Exercises/03 ExchangeIntegers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03ExchangeIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            int oldFirsNumber = firstNumber;
+            int oldSecondNumber = secondNumber;
+
+            firstNumber = secondNumber;
+            secondNumber = oldFirsNumber;
+
+            Console.WriteLine("Before:");
+            Console.WriteLine($"a = {oldFirsNumber}");
+            Console.WriteLine($"b = {oldSecondNumber}");
+            Console.WriteLine("After:");
+            Console.WriteLine($"a = {firstNumber}");
+            Console.WriteLine($"b = {secondNumber}");
+        }
+    }
+}
