53cd90aaf55b273d7227221c61a02c5c8a53cf2a
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 12:51:20 +0200

    Add files via upload

---
+++ 08. String-and-Text-Processing/Strings-and-Text-Processing-Lab/P05_DifitsLettersAndOther/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+namespace P05_DifitsLettersAndOther
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputText = Console.ReadLine();
+            string digits = "";
+            string letters = "";
+            string others = "";
+
+            for (int i = 0; i < inputText.Length; i++)
+            {
+                if (char.IsDigit(inputText[i]))
+                {
+                    digits += inputText[i];
+                }
+                else if (char.IsLetter(inputText[i]))
+                {
+                    letters += inputText[i];
+                }
+                else
+                {
+                    others += inputText[i];
+                }
+            }
+
+            Console.WriteLine(digits);
+            Console.WriteLine(letters);
+            Console.WriteLine(others);
+        }
+    }
+}
