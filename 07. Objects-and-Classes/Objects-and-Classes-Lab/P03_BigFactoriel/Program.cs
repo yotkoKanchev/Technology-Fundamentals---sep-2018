1cdc13093a5b268442670f6e97943a6c34a4ed37
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 12:22:25 +0200

    Add files via upload

---
+++ 07. Objects-and-Classes/Objects-and-Classes-Lab/P03_BigFactoriel/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Numerics;
+
+namespace P03._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            BigInteger factoriel = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                factoriel *= i;
+            }
+
+            Console.WriteLine(factoriel);
+        }
+    }
+}
