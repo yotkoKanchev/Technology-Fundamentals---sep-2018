1bb320e501f8c6da7b8ec1f2a44a5f737646efc3
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 12:48:23 +0200

    Add files via upload

---
+++ 08. String-and-Text-Processing/Strings-and-Text-Processing-More-Exercises/P05_HTML/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace P05_HTML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string title = Console.ReadLine();
+            string article = Console.ReadLine();
+
+            string inputLine = Console.ReadLine();
+            Console.WriteLine($"<h1>\n\t{title}\n</h1>");
+            Console.WriteLine($"<article>\n\t{article}\n</article>");
+            while (inputLine != "end of comments")
+            {
+                Console.WriteLine($"<div>\n\t{inputLine}\n</div>");
+
+
+                inputLine = Console.ReadLine();
+            }
+        }
+    }
+}
