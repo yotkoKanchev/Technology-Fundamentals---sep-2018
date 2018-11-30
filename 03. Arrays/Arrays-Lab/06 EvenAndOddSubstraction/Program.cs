370fb678393159df2ba6f3a21a7bb7d7663fd63d
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 11:02:08 +0200

    Add files via upload

---
+++ 03. Arrays/Arrays-Lab/06 EvenAndOddSubstraction/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace P06._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputLine = Console.ReadLine();
+
+            int[] evenNumsArray = inputLine
+                .Split()
+                .Select(int.Parse)
+                .Where(x => x % 2 == 0)
+                .ToArray();
+
+            int[] oddNumsArray = inputLine
+                .Split()
+                .Select(int.Parse)
+                .Where(x => x % 2 != 0)
+                .ToArray();
+
+            Console.WriteLine(evenNumsArray.Sum() - oddNumsArray.Sum());
+        }
+    }
+}
