a4baf9f43c299aac2ad729b76df10dfcb41fd069
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 11:40:45 +0200

    Add files via upload

---
+++ 04. Methods/Methods-Exercise/P09_ PalindromeIntegers/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace P09_9._PalindromeIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            while (input!= "END")
+            {
+                Console.WriteLine(FindPalindrome(input));
+
+                input = Console.ReadLine();
+            }
+        }
+
+        public static string FindPalindrome(string text)
+        {
+            string result = string.Empty;
+
+            StringBuilder reversedText = new StringBuilder();
+            for (int i = text.Length-1; i >=0 ; i--)
+            {
+                reversedText.Append(text[i]);
+            }
+
+            if (text == reversedText.ToString()) 
+            {
+                result = "true";
+            }
+            else
+            {
+                result = "false";
+            }
+            return result;
+        }
+    }
+}
