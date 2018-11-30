05ccb10f285cf177d30b81e9153c0c5debcb53f2
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 11:01:31 +0200

    Add files via upload

---
+++ 03. Arrays/Arrays-Lab/05 SumEvenNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace P05._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .Where(x => x % 2 == 0)
+                .ToArray();
+
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
