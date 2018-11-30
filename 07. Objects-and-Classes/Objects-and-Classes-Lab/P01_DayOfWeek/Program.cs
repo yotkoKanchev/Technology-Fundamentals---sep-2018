3caa2ac9bd8477eb2fead36b8ec7fcaf69b634b9
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 12:21:04 +0200

    Add files via upload

---
+++ 07. Objects-and-Classes/Objects-and-Classes-Lab/P01_DayOfWeek/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace P01._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString = Console.ReadLine();
+
+            DateTime dateAsString = DateTime.ParseExact(inputString, "d-M-yyyy", CultureInfo.InvariantCulture);
+
+            Console.WriteLine(dateAsString.DayOfWeek);
+        }
+    }
+}
