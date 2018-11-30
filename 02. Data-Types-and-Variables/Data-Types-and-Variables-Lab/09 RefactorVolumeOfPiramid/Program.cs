b06c243e8f5c3cec4849c156e7ed88d03cac2b61
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-11-30 09:56:26 +0200

    Add files via upload

---
+++ 02. Data-Types-and-Variables/Data-Types-and-Variables-Lab/09 RefactorVolumeOfPiramid/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09RefactorVolumeOfPiramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double lenght = 0;
+            double width = 0;
+            double heigth = 0;
+            Console.Write("Length: ");
+            lenght = double.Parse(Console.ReadLine());
+            Console.Write("Width: ");
+            width = double.Parse(Console.ReadLine());
+            Console.Write("Height: ");
+            heigth = double.Parse(Console.ReadLine());
+            double volume = (lenght * width * heigth) / 3;
+            Console.WriteLine("Pyramid Volume: {0:F2}", volume);
+
+        }
+    }
+}
