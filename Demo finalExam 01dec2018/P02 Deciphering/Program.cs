b7f0df105564ef571f987278d2951c2d9cb7c877
Yotko Kanchev <40663204+yotkoKanchev@users.noreply.github.com>
2018-12-01 16:39:58 +0200

    Add files via upload

---
+++ Demo finalExam 01dec2018/P02 Deciphering/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace P02_Deciphering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string encryptedMessage = Console.ReadLine();
+
+            string regex = @"^([d-z,|#{}]*)$";
+
+            var matches = Regex.Matches(encryptedMessage, regex);
+
+            string validEncrypedMessage = string.Empty;
+
+            foreach (Match match in matches)
+            {
+                validEncrypedMessage = match.Value;
+            }
+
+            if (validEncrypedMessage != string.Empty)
+            {
+                string reducedBy3Message = string.Empty;
+
+                foreach (var letter in encryptedMessage)
+                {
+                    reducedBy3Message += (char)((char)letter - 3);
+                }
+
+                string[] lettersOrSubstring = Console.ReadLine().Split();
+
+                string textToFind = lettersOrSubstring[0];
+                string textToChangeWith = lettersOrSubstring[1];
+
+                reducedBy3Message = reducedBy3Message.Replace(textToFind, textToChangeWith);
+
+                Console.WriteLine(reducedBy3Message);
+            }
+            else
+            {
+                Console.WriteLine("This is not the book you are looking for.");
+            }
+        }
+    }
+}
